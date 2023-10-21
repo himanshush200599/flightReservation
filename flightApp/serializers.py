84e288575e9839d5921b801da9cdc30266bb7033
himanshush200599 <himanshu.sharma@airbase.io>
2023-10-22 00:10:47 +0530

    Flight Reservation: Add flight reservation APIs

---
+++ flightApp/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from flightApp.models import Flight, Passenger, Reservation
+import re
+
+
+class FlightSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Flight
+        fields = '__all__'
+
+    # can write custom validator like this syntax --> `validate_${modelKey}``
+    def validate_flightNumber(self, flightNumber):
+        if (re.match("^[a-zA-Z0-9]*$", flightNumber) == None):
+            raise serializers.ValidationError(
+                "Invalid flight number, only accepted is alphanumeric")
+
+        return flightNumber
+
+    # if we need all the data togrther can use `validate`` function
+    def validate(self, data):
+        return data
+
+
+class PassengerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Passenger
+        fields = '__all__'
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Reservation
+        fields = '__all__'
